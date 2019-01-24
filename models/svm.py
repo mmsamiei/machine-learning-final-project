40572c3f411e7b542116e71bd96c4aac377a9cc8
mmsamiei <mohmahsamiei@gmail.com>
2019-01-24 14:01:53 +0330

    add svm with l1 and l2 penalty analytics, dedicated to *a******bg

--- models/svm.py
+++ models/svm.py
@@ -5,7 +5,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RandomizedSearchCV
-
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV
 
 def without_penalty():
     X = numpy.loadtxt("../data/Train/X_train.txt")
@@ -30,6 +31,29 @@ def without_penalty():
     print(scores)
     print(numpy.mean(scores))
 
+def penalty_l1():
+    X = numpy.loadtxt("../data/Train/X_train.txt")
+    y = numpy.loadtxt("../data/Train/y_train.txt")
+    X = feature_selection.dummy_selctor(X, 300)
+    clf = LinearSVC(penalty='l1', dual=False)
+    C = [1e-4, 2e-4, 5e-4, 1e-3, 2e-3, 5e-3, 1e-2, 1e-1, 1, 1e1, 1e2]
+    complete_grid = {
+        'C': C,
+    }
+    grid_search = GridSearchCV(estimator=clf, param_grid=complete_grid,
+                               cv=3, n_jobs=-1)
+    grid_search.fit(X, y)
+    best_grid = grid_search.best_estimator_
+    print(best_grid.get_params())
+    scores = cross_val_score(best_grid, X, y, cv=5)
+    print("cv scores of the best svm is:")
+    print(scores)
+    print('mean scores of cv scores are:  {:0.3f}%.'.format(100 * numpy.mean(scores)))
+    coef = best_grid.coef_
+    print(temp)
+    print(all)
 
 if __name__ == '__main__':
-    without_penalty()
+    #without_penalty()
+    penalty_l1()
+
--- src/preprocessing/feature_selection.py
+++ src/preprocessing/feature_selection.py
@@ -1,3 +1,5 @@
 def dummy_selctor(X, l):
+    if( l == 'all'):
+        return X
     data = X.T[0:l].T
     return data
